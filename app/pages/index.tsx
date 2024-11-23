da06c771da7d63d260336fab293da4d18d90475f
Shubh1810 <your-github-email@example.com>
2024-11-22 17:05:56 -0800

    feat: deployment updates

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,9 +1,9 @@
 // pages/index.tsx
 import React from 'react';
-import Navbar from '@radix-ui/react-slot/app/Components/Navbar/Navbar';
-import Footer from '@radix-ui/react-slot/app/Components/Footer/Footer';
-import styles from '@radix-ui/react-slot/app/styles/Home.module.css';
-import HeroSection from '@radix-ui/react-slot/app/Components/HeroSection/HeroSection';
+import Navbar from '../Components/Navbar/Navbar';
+import Footer from '../Components/Footer/Footer';
+import styles from '../styles/Home.module.css';
+import HeroSection from '../Components/HeroSection/HeroSection';
 const Home: React.FC = () => {
   return (
     <div className={styles.viewport}>
