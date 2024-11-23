8a5c448f54693b356736bfdb06f7c38b14e545f3
Shubh1810 <your-github-email@example.com>
2024-11-23 15:19:47 -0800

    feat: updates

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -6,10 +6,6 @@ import styles from '../styles/Home.module.css';
 import HeroSection from '../Components/HeroSection/HeroSection';
 import { ParallaxScroll } from '../Components/Common/ui/parallax-scroll';
 
-export function ParallaxScrollDemo() {
-  return <ParallaxScroll images={images} />;
-}
-
 const images = [
   "https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
   "https://images.unsplash.com/photo-1505144808419-1957a94ca61e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3070&q=80",
@@ -37,12 +33,13 @@ const images = [
   "https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
   "https://images.unsplash.com/photo-1505144808419-1957a94ca61e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3070&q=80",
 ];
+
 const Home: React.FC = () => {
   return (
     <div className={styles.viewport}>
       <HeroSection />
       <main className={styles.main}>
-        <ParallaxScroll />
+        <ParallaxScroll images={images} />
 
         {/* Other sections */}
       </main>
